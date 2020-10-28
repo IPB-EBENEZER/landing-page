d92febca07ff922c11d627aad6b8c507d588bb9e
Yago Lopes <yagolopeslazaro@gmail.com>
2020-10-28 18:49:22 -0300

    add lista de aniversariantes

---
+++ data.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    name: "Yago Lopes Lázaro",
+    date: "10-20-1996",
+    profile: "@yagolopes",
+  },
+  {
+    name: "Itallo Dornelas",
+    date: "07-10-1996",
+    profile: "@itallo",
+  },
+  {
+    name: "Thiago Eustáquio",
+    date: "05-17-1996",
+    profile: "@yagolopes",
+  },
+];
